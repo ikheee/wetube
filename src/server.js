2e0892cbcfafc8a4fd64108cdbbf6dc60655e849
shinikhee <ikheee@naver.com>
2023-06-13 03:19:50 +0900

    미들웨어

--- src/server.js
+++ src/server.js
@@ -5,8 +5,24 @@ const app = express();
 
 const port = 4000;
 
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
+
+const privateMiddleware = (req, res, next) => {
+  if (req.url === '/protected') {
+    return res.send('<h1>Not Allowed</h1>');
+    // } else {
+    //   res.status(401).send('Unauthorized');
+  }
+  next();
+};
+
 const handleListenging = () =>
   console.log(`Listening on: http://localhost:${port}`);
+
+app.use(logger, privateMiddleware);
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
